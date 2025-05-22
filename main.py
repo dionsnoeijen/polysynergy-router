0c05a2f84fe8f9b0f508e493a779cfdafae4e837
Dion Snoeijen <hallo@dionsnoeijen.nl>
2025-05-22 11:43:25 +0200

    Make deployable

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from api.crud import router as crud_router
 from api.router import router as routing_router
 
 app = FastAPI()
-app.include_router(crud_router)
+app.include_router(crud_router, prefix="/__router-internal")
 app.include_router(routing_router)
 
+@app.get("/__internal/health")
+def health():
+    return {"ok": True}
